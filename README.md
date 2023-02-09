4c6684d6863b32c025d2f76adf840383cd1e86c7
Hassan Magdy <hasan.magdy@gmail.com>
2023-02-09 20:04:48 +0200

    feat: demo is working

---
+++ README.md
@@ -0,0 +1,4 @@
+# google-contact-demo
+ - add credentials.json
+ - run: `node index.js`
+ - make sure you are using an email with admin access
---
+++ index.js
@@ -0,0 +1,94 @@
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/admin.directory.user'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  console.log(client);
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Lists the first 10 users in the domain.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listUsers(auth) {
+  const service = google.admin({version: 'directory_v1', auth});
+  const res = await service.users.list({
+    customer: 'my_customer',
+    maxResults: 10,
+    orderBy: 'email',
+  });
+
+  const users = res.data.users;
+  if (!users || users.length === 0) {
+    console.log('No users found.');
+    return;
+  }
+
+  console.log('Users:');
+  users.forEach((user) => {
+    console.log(`${user.primaryEmail} (${user.name.fullName})`);
+  });
+}
+
+authorize().then(listUsers).catch(console.error);
